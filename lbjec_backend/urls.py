b4ad22c07e31304e838649124353916622632c69
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-16 13:42:30 +0800

    feat(urls): added customer and token endpoints

--- lbjec_backend/urls.py
+++ lbjec_backend/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+api_prefix = "api/v1/"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(f"{api_prefix}customer/", include("customer.urls")),
+    path(
+        f"{api_prefix}token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        f"{api_prefix}token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
 ]
