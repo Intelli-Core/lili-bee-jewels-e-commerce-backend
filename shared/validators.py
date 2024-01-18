7121c53e3158c628bacdcb9dbcbd3bf3f5ff8858
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-18 17:20:12 +0800

    feat(shared): created shared features

---
+++ shared/validators.py
@@ -0,0 +1,17 @@
+import os
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+def validate_image(value):
+    from django.core.exceptions import ValidationError
+
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = [".jpg", ".png"]
+    if ext.lower() not in valid_extensions:
+        raise ValidationError("Unsupported file extension.")
+
+
+def validate_date(value):
+    if value < timezone.now().date():
+        raise ValidationError("Date cannot be in the past")
