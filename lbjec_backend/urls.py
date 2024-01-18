bee2acdaaed632bab2b81d6cb468413cbed5f04e
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-18 17:26:20 +0800

    refactor(urls): added swagger and product endpoints

--- lbjec_backend/urls.py
+++ lbjec_backend/urls.py
@@ -15,21 +15,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import include, path, re_path
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
 
 api_prefix = "api/v1/"
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Lili Bee Jewels E-Commerce API",
+        default_version="v1",
+        description="API for Lili Bee Jewels E-Commerce",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
+    path(f"{api_prefix}auth/", include("shared.auth.urls")),
     path(f"{api_prefix}customer/", include("customer.urls")),
-    path(
-        f"{api_prefix}token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
-    ),
-    path(
-        f"{api_prefix}token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
-    ),
+    path(f"{api_prefix}product/", include("product.urls")),
 ]
