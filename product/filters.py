e077baeb0bf9c02d27f18a557b8b859422bf69f9
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-19 22:25:24 +0800

    refactor(product): updated material filter

--- product/filters.py
+++ product/filters.py
@@ -5,7 +5,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from django_filters import rest_framework as filters
 from django_filters.constants import EMPTY_VALUES
-from product.models import Product, ProductAttributes, ProductCategory
+from product.models import Product, ProductAttributes, ProductCategory, ProductMaterial
 
 
 class ArrayFilter(filters.Filter):
@@ -17,7 +17,7 @@ class ArrayFilter(filters.Filter):
 
 class ProductFilter(filters.FilterSet):
     category = filters.CharFilter(field_name="category__name", lookup_expr="iexact")
-    material = filters.CharFilter(field_name="attributes__material", lookup_expr="iexact")
+    material = filters.CharFilter(field_name="attributes__material__name", lookup_expr="iexact")
     size = ArrayFilter(field_name="attributes__sizes")
 
     class Meta:
@@ -29,14 +29,14 @@ class FilterOptionsListView(generics.ListAPIView):
     def get(self, request):
         categories = (
             ProductCategory.objects.annotate(category_name=Lower("name"))
-            .values("category_name")
+            .values("id", "category_name")
             .annotate(count=Count("product"))
             .order_by("category_name")
         )
         materials = (
-            Product.objects.annotate(material_name=Lower(F("attributes__material")))
-            .values("material_name")
-            .annotate(count=Count("id"))
+            ProductMaterial.objects.annotate(material_name=Lower("name"))
+            .values("id", "material_name")
+            .annotate(count=Count("attributes"))
             .order_by("material_name")
         )
         sizes = ProductAttributes.objects.values_list("sizes", flat=True)
@@ -44,6 +44,12 @@ class FilterOptionsListView(generics.ListAPIView):
         size_counts = dict(Counter(all_sizes))
         # Sort sizes
         size_counts = {k: size_counts[k] for k in sorted(size_counts)}
+
+        # Capitalize the first letter of each word in category_name and material_name
+        categories = [{**category, 'category_name': category['category_name'].title(), 'id': category['id']} for
+                      category in categories]
+        materials = [{**material, 'material_name': material['material_name'].title(), 'id': material['id']} for material
+                     in materials]
 
         return Response(
             {
