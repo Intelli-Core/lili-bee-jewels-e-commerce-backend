8ff650e416923df9350d8a51d6fc475c82c4c26f
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-19 18:55:38 +0800

    refactor(product): changed metal field to material field

--- product/models.py
+++ product/models.py
@@ -17,7 +17,7 @@ class ProductAttributes(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    metal = models.CharField(max_length=255, blank=False, null=False)
+    material = models.CharField(max_length=255, blank=False, null=False)
     weight = models.DecimalField(
         max_digits=10, decimal_places=2, blank=False, null=False
     )
