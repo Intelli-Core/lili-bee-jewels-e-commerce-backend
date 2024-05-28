d430c21403bf10310b7a59047daf8b22714f7be2
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-05-28 21:34:35 +0800

    LBJEC-20 refactor(shared/generic_viewset): updated

--- shared/generic_viewset.py
+++ shared/generic_viewset.py
@@ -9,9 +9,9 @@ class GenericViewSet(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
-    mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
+    include_list_view = True  # Add this line
     protected_views = []
     permissions = [AllowAny]  # Default permission
 
@@ -19,16 +19,13 @@ class GenericViewSet(
         if self.action in self.protected_views:
             return [permission() for permission in self.permissions]
         return [AllowAny()]
-    
+
     def list(self, request, *args, **kwargs):
+        if not self.include_list_view:  # Add this check
+            return Response(status=405)  # 405 Method Not Allowed
         queryset = self.filter_queryset(self.get_queryset())
-
-        # Get the limit from the query parameters (with a default)
-        limit = int(request.query_params.get('limit', 100))
-
-        # Use Django's Paginator to apply the limit
+        limit = int(request.query_params.get("limit", 100))
         paginator = Paginator(queryset, limit)
         page = paginator.get_page(1)
-
         serializer = self.get_serializer(page, many=True)
         return Response(serializer.data)
