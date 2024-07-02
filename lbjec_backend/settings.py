12709274e59e2854ea645a3ac1bb6bb0b6627282
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-07-02 17:41:33 +0800

    fix(settings): fixed static files error when in prod

--- lbjec_backend/settings.py
+++ lbjec_backend/settings.py
@@ -182,20 +182,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-if DEBUG:
-    STATIC_ROOT = "/static/"
-    STATIC_URL = "/static/"
-else:
-    STATIC_ROOT = config("STATIC_ROOT")
-    STATIC_URL = config("STATIC_URL")
-
-if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-    MEDIA_URL = "/media/"
-else:
-    MEDIA_ROOT = os.path.join(BASE_DIR, config("MEDIA_ROOT"))
-    MEDIA_URL = config("MEDIA_URL")
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
