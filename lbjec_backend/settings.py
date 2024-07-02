62619db8ee07254104108ff0a5a6a7b8b55fdb32
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-07-02 17:51:04 +0800

    refactor(settings): fixed allowed hosts and added whitenoise middleware

--- lbjec_backend/settings.py
+++ lbjec_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["https://lbjec-backend-production.up.railway.app"]
+ALLOWED_HOSTS = ["lbjec-backend-production.up.railway.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "lbjec_backend.urls"
